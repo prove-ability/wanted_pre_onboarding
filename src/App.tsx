93ee0783c589b01899aabe07d2ac06901f75fc02
Bard <prove.ability00@gmail.com>
2022-01-16 12:43:49 +0900

    💄 css reset lib 추가 브라우저 간 동일한 디자인을 위해

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 // import React from "react";
 // import logo from "./logo.svg";
 // import "./App.css";
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
 import Slice from "./components/common/Slice";
 import Layout from "./components/Layout";
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+ 
+`;
+
 function App() {
 	return (
-		<div>
+		<>
+			<GlobalStyle />
 			<Layout>
 				<Slice />
 			</Layout>
-		</div>
+		</>
 	);
 }
 
